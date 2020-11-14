dd013d408119cc160880b7b20733de623aecdf83
Hernan Chamorro <lc.hernanchamorro@gmail.com>
2020-11-14 20:09:55 -0300

    creacion componente Footer

---
+++ basics/componentes/Footer.js
@@ -0,0 +1,11 @@
+app.component('footer-banco', {
+    //template lleva html dentro
+    template:/*html*/`
+        <div class="bg-dark py-3 mt-2 text-white">
+            <h3>Footer de mi sitio web</h3>
+        </div>
+        `
+})
+//El primer parametro es el nombre, el segundo un objeto con las propiedades que quiero pasar.
+//funcion siempre y cuando tenga creada la constante de app
+//nombre del componente siempre 
